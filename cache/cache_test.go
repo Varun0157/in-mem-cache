29f74a3314eab5ee0bb7cd901890dffc333fa78d
Varun0157 <varun.edachali@gmail.com>
2025-06-14 11:55:31 +0530

    more robust testing

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -111,3 +111,137 @@ func BenchmarkCacheGet(b *testing.B) {
 		c.Get(i % 1000)
 	}
 }
+
+func TestCache_ZeroCapacity(t *testing.T) {
+	// Should default to capacity 1
+	c := cache.New[string, int](0, policies.NewLRU[string]())
+
+	c.Set("a", 1)
+	c.Set("b", 2) // Should evict "a"
+
+	_, found := c.Get("a")
+	require.False(t, found)
+
+	val, found := c.Get("b")
+	require.True(t, found)
+	require.Equal(t, 2, val)
+}
+
+func TestCache_UpdateExistingKey(t *testing.T) {
+	c := cache.New[string, int](2, policies.NewLRU[string]())
+
+	c.Set("a", 1)
+	c.Set("a", 2) // Update existing key
+
+	val, found := c.Get("a")
+	require.True(t, found)
+	require.Equal(t, 2, val)
+}
+
+func TestCache_Delete(t *testing.T) {
+	c := cache.New[string, int](2, policies.NewLRU[string]())
+
+	c.Set("a", 1)
+	c.Set("b", 2)
+
+	// Delete existing key
+	c.Delete("a")
+	_, found := c.Get("a")
+	require.False(t, found)
+
+	// Delete non-existent key should not panic
+	c.Delete("c")
+
+	// Other key should still exist
+	val, found := c.Get("b")
+	require.True(t, found)
+	require.Equal(t, 2, val)
+}
+
+func TestCache_ComplexValueType(t *testing.T) {
+	type ComplexValue struct {
+		ID    int
+		Data  []string
+		Extra map[string]interface{}
+	}
+
+	c := cache.New[string, ComplexValue](2, policies.NewLRU[string]())
+
+	val1 := ComplexValue{
+		ID:    1,
+		Data:  []string{"test1", "test2"},
+		Extra: map[string]interface{}{"key": 123},
+	}
+
+	val2 := ComplexValue{
+		ID:    2,
+		Data:  []string{"test3"},
+		Extra: map[string]interface{}{"key": "value"},
+	}
+
+	c.Set("a", val1)
+	c.Set("b", val2)
+
+	// Test retrieval
+	retrieved, found := c.Get("a")
+	require.True(t, found)
+	require.Equal(t, val1.ID, retrieved.ID)
+	require.Equal(t, val1.Data, retrieved.Data)
+	require.Equal(t, val1.Extra, retrieved.Extra)
+}
+
+func TestCache_ConcurrentDelete(t *testing.T) {
+	c := cache.New[int, int](100, policies.NewLRU[int]())
+	var wg sync.WaitGroup
+	numGoroutines := 10
+	numOperations := 1000
+
+	// First populate the cache
+	for i := 0; i < numGoroutines*numOperations; i++ {
+		c.Set(i, i)
+	}
+
+	for i := range numGoroutines {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			for j := range numOperations {
+				key := id*numOperations + j
+				c.Delete(key)
+				_, found := c.Get(key)
+				require.False(t, found)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+func TestCache_ConcurrentMixedOperations(t *testing.T) {
+	c := cache.New[int, int](100, policies.NewLRU[int]())
+	var wg sync.WaitGroup
+	numGoroutines := 10
+	numOperations := 1000
+
+	for i := range numGoroutines {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			for j := range numOperations {
+				key := id*numOperations + j
+
+				// Mix of operations
+				switch j % 3 {
+				case 0:
+					c.Set(key, key)
+				case 1:
+					c.Get(key)
+				case 2:
+					c.Delete(key)
+				}
+			}
+		}(i)
+	}
+
+	wg.Wait()
+}
